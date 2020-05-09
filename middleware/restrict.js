9b206641e297fbb9def0876f6d02abc2ab17b7b1
tanveersaleem786 <tanveer.saleem786@gmail.com>
2020-05-09 19:07:36 -0400

    HIT MVP and Complete Stretch Task#1

---
+++ middleware/restrict.js
@@ -0,0 +1,47 @@
+const jwt = require("jsonwebtoken")
+
+function restrict() {
+
+    return async (req, res, next) => {
+       
+        const authError = {
+            message: "Invalid Credentials"
+        }
+       
+        try {
+            // this utilizes the `cookie-parser` middleware to pull the JWT out
+            // of the cookies that were automatically sent by the client.
+           
+            const token = req.cookies.token
+            if(!token)
+              res.status(401).json(authError)
+             
+                       
+            // this checks to make sure the token's signature is valid. if it is,
+			// we can trust the data in the payload and consider the user logged in.
+			// if it isn't, we know the payload may have been tampered with, and we
+			// make the user log in again.
+			jwt.verify(token, process.env.JWT_SECRET, (err, decodedPayload) => {
+                //
+                if (err) {
+					return res.status(401).json(authError)
+				}
+
+				// we attach the decoded payload values to the request, just in case we
+				// need to access them in later middleware functions or route handlers.
+				// (to look up the user by ID, for example.)
+				req.token = decodedPayload
+
+				next()
+			})
+
+       
+        } catch(err) {
+            next(err)
+        }
+
+    }
+}
+
+
+module.exports =  restrict
